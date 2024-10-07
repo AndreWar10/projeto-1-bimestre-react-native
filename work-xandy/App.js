85970fd1c2969e8f9f4fa916b05001470bb0f6ac
AndreGuerra <andreguerra@lojadomecanico.com.br>
2024-10-06 23:50:01 -0300

    :fix -> correction of App.js

--- work-xandy/App.js
+++ work-xandy/App.js
@@ -6,21 +6,18 @@ import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import CardsScreen from './src/screens/CardsScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
-import { FavoritesProvider } from './src/contexts/FavoritesContext'; // Importando o contexto
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <FavoritesProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-          <Stack.Screen name="Cards" component={CardsScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </FavoritesProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Cards" component={CardsScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
